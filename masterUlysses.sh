4c07ee7325dbde3b58cba2194cbd3d490c32e919
Federico Balducci <fbalducc@login2.mgmt>
2021-11-09 14:16:47 +0100

    bug in master

--- masterUlysses.sh
+++ masterUlysses.sh
@@ -3,11 +3,8 @@ do
     
     for eps in 0.5 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0;
     do
-        cp runUlysses.sh temp.sh
-        
-        sed -i "" "s/ENNE/$N/g" temp.sh        
-        sed -i "" "s/EPSILON/$eps/g" temp.sh        
-        
+        sed "s/ENNE/$N/g;s/EPSILON/$eps/g" runUlysses.sh > temp.sh        
+                
         sbatch temp.sh
         
         sleep 1
@@ -15,3 +12,4 @@ do
 done
 
 rm temp.sh
+
